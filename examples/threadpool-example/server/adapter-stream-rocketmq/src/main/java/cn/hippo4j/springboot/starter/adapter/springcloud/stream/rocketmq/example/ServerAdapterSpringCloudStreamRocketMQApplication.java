667258e6233025601a5080e02cedd18fb5efc091
在小菜 <1275952454@qq.com>
2024-08-01 10:41:01 +0800

    nacos版本支持springboot3

--- examples/threadpool-example/server/adapter-stream-rocketmq/src/main/java/cn/hippo4j/springboot/starter/adapter/springcloud/stream/rocketmq/example/ServerAdapterSpringCloudStreamRocketMQApplication.java
+++ examples/threadpool-example/server/adapter-stream-rocketmq/src/main/java/cn/hippo4j/springboot/starter/adapter/springcloud/stream/rocketmq/example/ServerAdapterSpringCloudStreamRocketMQApplication.java
@@ -21,15 +21,15 @@ import cn.hippo4j.core.enable.EnableDynamicThreadPool;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.stream.annotation.EnableBinding;
-import org.springframework.cloud.stream.messaging.Source;
+//import org.springframework.cloud.stream.annotation.EnableBinding;
+//import org.springframework.cloud.stream.messaging.Source;
 
 /**
  * Server Adapter Spring Cloud Stream RocketMQ Application
  */
 @Slf4j
 @EnableDynamicThreadPool
-@EnableBinding({Source.class, MySink.class})
+//@EnableBinding({Source.class, MySink.class})
 @SpringBootApplication
 public class ServerAdapterSpringCloudStreamRocketMQApplication {
 
